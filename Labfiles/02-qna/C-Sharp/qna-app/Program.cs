cbbaf4959b3e9ca855676b8d827d29307d34c3e6
Paul McNamara <paulmac445@gmail.com>
2025-06-04 15:41:07 -0400

    Added csharp for language service and knowledge bank tests

--- Labfiles/02-qna/C-Sharp/qna-app/Program.cs
+++ Labfiles/02-qna/C-Sharp/qna-app/Program.cs
@@ -2,6 +2,8 @@
 using System.IO;
 using System.Text;
 using Microsoft.Extensions.Configuration;
+using Azure;
+using Azure.AI.Language.QuestionAnswering;
 
 // Import namespaces
 
@@ -23,10 +25,29 @@ namespace qna_app
                 string deploymentName = configuration["QADeploymentName"];
 
                 // Create client using endpoint and key
-
+                AzureKeyCredential credentials = new(aiSvcKey);
+                Uri endpoint = new Uri(aiSvcEndpoint);
+                QuestionAnsweringClient aiClient = new(endpoint, credentials);
 
                 // Submit a question and display the answer
-                
+                string user_question = "";
+                while (true)
+                {
+                    Console.WriteLine("Question: ");
+                    user_question = Console.ReadLine();
+                    if (user_question.ToLower() == "quit")
+                        break;
+                    QuestionAnsweringProject project = new QuestionAnsweringProject(projectName, deploymentName);
+                    Response<AnswersResult> response = aiClient.GetAnswers(user_question, project);
+                    foreach (KnowledgeBaseAnswer answer in response.Value.Answers)
+                    {
+                        Console.WriteLine(answer.Answer);
+                        Console.WriteLine($"Confidence: {answer.Confidence:P2}");
+                        Console.WriteLine($"Source: {answer.Source}");
+                        Console.WriteLine();
+                    }   
+
+                }
 
             }
             catch (Exception ex)
